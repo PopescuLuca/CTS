5c894ffe195d080cd158b192a2a42f200f28fd17
PopescuLuca <popesculuca18@stud.ase.ro>
2021-05-22 11:17:36 +0300

    G-unit teste (a doua parte)

--- Seminar12/src/cts/ase/ro/tests/StudentTests.java
+++ Seminar12/src/cts/ase/ro/tests/StudentTests.java
@@ -33,4 +33,56 @@ public class StudentTests {
         assertEquals(8,student.getNota(1));
         assertEquals(6,student.getNota(2));
     }
+    @Test
+    public void testDimensiuneLista(){
+        Student student=new Student();
+        student.adaugaNota(10);
+        student.adaugaNota(9);
+        assertEquals(2,student.getNote().size());
+    }
+    @Test
+    public void testCalculeazaMedie(){
+        Student student=new Student();
+        student.adaugaNota(10);
+        student.adaugaNota(7);
+        assertEquals(8.5f,student.calculeazaMedie(),0.1);
+    }
+    @Test
+    public void testRestante(){
+        Student student=new Student();
+        student.adaugaNota(4);
+        student.adaugaNota(7);
+        assertTrue(student.areRestante());
+    }
+    @Test
+    public void testfaraRestante(){
+        Student student=new Student();
+        student.adaugaNota(6);
+        assertFalse(student.areRestante());
+    }
+    @Test
+    public void testgetNotaInafaraLimitei(){
+        Student student=new Student();
+        student.adaugaNota(10);
+        //1
+        try {
+            //2
+            student.getNota(-1);
+            //3--nu se ajunge
+            fail("Nu se ajunge aici. Metoda arunca exceptie");
+        }catch (IndexOutOfBoundsException exception){
+            //4
+        }
+        catch (Exception exception){
+            //5--nu se ajunge
+            fail("Tipul exceptiei aruncat nu este corect");
+        }
+        //6
+    }
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetNotaInvalida(){
+        Student student=new Student();
+        student.getNota(10);
+        int nota=student.getNota(-1);
+    }
 }
